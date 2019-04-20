dc24937161114da33b68be3f0790bfb5e87b68ba
frank870622 <frank870622@gmail.com>
2019-04-20 12:03:28 +0800

    add multipage check

--- Question1.py
+++ Question1.py
@@ -1,39 +1,52 @@
 import urllib.request
 import re
 import matplotlib.pyplot as plt
+import math
 
 # a dictionary to save the number of the pagpers been publish each year of an author
 datelist = {}
 # let user to input author name
 author_name = input("Input Author: ")
-
 #to use the author search to get html, replace ' ' in author name with '+'
 author = author_name.replace(' ', '+')
+
 #set the url we want
 url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
 #get the html file content of arxiv
 content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
 
-#a re pattern to get the papers number of this author search (not use)
-#page_pattern = 'of [0-9]* results for author:'
-#paper_num = re.findall(page_pattern, html_str)
+#a re pattern to get the papers number of this author search
+page_pattern = 'of [0-9]* results for author:'
+paper_num = re.findall(page_pattern, html_str)
+page_num = paper_num[0].split('of ')[1].split(" results")[0].strip()
+page_num = int(page_num)
+#the result page number we want
+page_num = math.ceil(page_num/50)
 
-#a re pattern to get the originally announced date of each paper
-date_pattern = 'originally announced</span>[\s\S]*?</p>'
-#get string of announced date of each paper
-date = re.findall(date_pattern, html_str)
+#search by pages
+for i in range(0, page_num):
+    # set the url we want (each page)
+    url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(i*50)
+    # get the html file content of arxiv
+    page_content = urllib.request.urlopen(url)
+    page_html_str = page_content.read().decode('utf-8')
 
-for t in date:
-    # cut the string of announced date(month & year)
-    date = t.split('</span>')[1].split(".")[0].strip()
-    #split date by ' ' to get the year of paper announced date
-    date = date.split(' ')[1]
-    #add this result to the date dictionary
-    if date in datelist:
-        datelist[date] += 1
-    else:
-        datelist[date] = 1
+    #a re pattern to get the originally announced date of each paper
+    date_pattern = 'originally announced</span>[\s\S]*?</p>'
+    #get string of announced date of each paper
+    date = re.findall(date_pattern, page_html_str)
+
+    for t in date:
+        # cut the string of announced date(month & year)
+        date = t.split('</span>')[1].split(".")[0].strip()
+        #split date by ' ' to get the year of paper announced date
+        date = date.split(' ')[1]
+        #add this result to the date dictionary
+        if date in datelist:
+            datelist[date] += 1
+        else:
+            datelist[date] = 1
 
 #sort the result by dictionary's key
 datelist_key = list(datelist.keys())
--- Question2.py
+++ Question2.py
@@ -1,41 +1,54 @@
 import urllib.request
 import re
+import math
 
 # a dictionary to save the number of the co-author appear in all papers
 co_author_list = {}
 # let user to input author name
 author_name = input("Input Author: ")
-
 #to use the author search to get html, replace ' ' in author name with '+'
 author = author_name.replace(' ', '+')
+
 #set the url we want
 url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
 #get the html file content of arxiv
 content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
 
-#a re pattern to get the papers number of this author search (not use)
-#page_pattern = 'of [0-9]* results for author:'
-#paper_num = re.findall(page_pattern, html_str)
+#a re pattern to get the papers number of this author search
+page_pattern = 'of [0-9]* results for author:'
+paper_num = re.findall(page_pattern, html_str)
+page_num = paper_num[0].split('of ')[1].split(" results")[0].strip()
+page_num = int(page_num)
+#the result page number we want
+page_num = math.ceil(page_num/50)
 
-#a re pattern to get the co-author of each paper
-co_author_line_pattern = 'Authors:</span>[\s\S]*?</p>'
-#get string of co-author of each paper
-co_author_line = re.findall(co_author_line_pattern, html_str)
+#search by pages
+for i in range(0, page_num):
+    # set the url we want (each page)
+    url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(i*50)
+    # get the html file content of arxiv
+    page_content = urllib.request.urlopen(url)
+    page_html_str = page_content.read().decode('utf-8')
 
-#a re pattern to get each co-author of a paper
-co_author_pattern = '\">[\s\S]*?</a>'
-for t in co_author_line:
-    # get each co-author of a paper
-    co_author = re.findall(co_author_pattern, t)
-    for t in co_author:
-        # cut the string of co-author
-        co = t.split('\">')[1].split("</a>")[0].strip()
-        # add this result to the co-author dictionary
-        if co in co_author_list:
-            co_author_list[co] += 1
-        else:
-            co_author_list[co] = 1
+    #a re pattern to get the co-author of each paper
+    co_author_line_pattern = 'Authors:</span>[\s\S]*?</p>'
+    #get string of co-author of each paper
+    co_author_line = re.findall(co_author_line_pattern, html_str)
+
+    #a re pattern to get each co-author of a paper
+    co_author_pattern = '\">[\s\S]*?</a>'
+    for t in co_author_line:
+        # get each co-author of a paper
+        co_author = re.findall(co_author_pattern, t)
+        for t in co_author:
+            # cut the string of co-author
+            co = t.split('\">')[1].split("</a>")[0].strip()
+            # add this result to the co-author dictionary
+            if co in co_author_list:
+                co_author_list[co] += 1
+            else:
+                co_author_list[co] = 1
 
 #print the co-author's name and times (co-author's name sorted by alphabet)
 #the result won't print the author name you search
